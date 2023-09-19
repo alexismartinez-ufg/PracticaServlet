47bdab002376c7b40819804a338b7e26e951c4c7
AlexisMartínez <ia.alexismartinez@ufg.edu.sv>
2023-09-18 22:53:28 -0600

    Metodo delete

---
+++ target/generated-sources/annotations/Logica/Estudiante_.java
@@ -0,0 +1,20 @@
+package Logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2023-09-18T22:46:15")
+@StaticMetamodel(Estudiante.class)
+public class Estudiante_ { 
+
+    public static volatile SingularAttribute<Estudiante, String> municipio;
+    public static volatile SingularAttribute<Estudiante, String> encargado;
+    public static volatile SingularAttribute<Estudiante, String> apellido;
+    public static volatile SingularAttribute<Estudiante, Integer> id;
+    public static volatile SingularAttribute<Estudiante, String> nie;
+    public static volatile SingularAttribute<Estudiante, String> sexo;
+    public static volatile SingularAttribute<Estudiante, String> telefono;
+    public static volatile SingularAttribute<Estudiante, String> nombre;
+
+}
